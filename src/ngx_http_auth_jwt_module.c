3c6e63a5e2f7c47e1e876a96e52182eda2c7ad9b
bpostlethwaite <post.ben.here@gmail.com>
2017-09-08 17:23:17 -0400

    add jwt error code

--- src/ngx_http_auth_jwt_module.c
+++ src/ngx_http_auth_jwt_module.c
@@ -97,22 +97,22 @@ static ngx_int_t ngx_http_auth_jwt_handler(ngx_http_request_t *r)
 	jwt_alg_t alg;
 	time_t exp;
 	time_t now;
-	
+
 	jwtcf = ngx_http_get_module_loc_conf(r, ngx_http_auth_jwt_module);
-	
-	if (!jwtcf->auth_jwt_enabled) 
+
+	if (!jwtcf->auth_jwt_enabled)
 	{
 		return NGX_DECLINED;
 	}
-	
-//	ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Key: %s, Enabled: %d", 
-//			jwtcf->auth_jwt_key.data, 
+
+//	ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Key: %s, Enabled: %d",
+//			jwtcf->auth_jwt_key.data,
 //			jwtcf->auth_jwt_enabled);
-	
+
 	// get the cookie
 	// TODO: the cookie name could be passed in dynamicallly
 	n = ngx_http_parse_multi_header_lines(&r->headers_in.cookies, &jwtCookieName, &jwtCookieVal);
-	if (n == NGX_DECLINED) 
+	if (n == NGX_DECLINED)
 	{
 		// if we can't find the first cookie, check the legacy location
 		n = ngx_http_parse_multi_header_lines(&r->headers_in.cookies, &passportKeyCookieName, &jwtCookieVal);
@@ -122,12 +122,12 @@ static ngx_int_t ngx_http_auth_jwt_handler(ngx_http_request_t *r)
 			goto redirect;
 		}
 	}
-	
+
 	// the cookie data is not necessarily null terminated... we need a null terminated character pointer
 	jwtCookieValChrPtr = ngx_str_t_to_char_ptr(r->pool, jwtCookieVal);
 
 //	ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "rampartjwt: %s %d", jwtCookieValChrPtr, jwtCookieVal.len);
-	
+
 	// convert key from hex to binary
 	keyBinary = ngx_palloc(r->pool, jwtcf->auth_jwt_key.len / 2);
 	if (0 != hex_to_binary((char *)jwtcf->auth_jwt_key.data, keyBinary, jwtcf->auth_jwt_key.len))
@@ -135,17 +135,17 @@ static ngx_int_t ngx_http_auth_jwt_handler(ngx_http_request_t *r)
 		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "failed to turn hex key into binary");
 		goto redirect;
 	}
-	
+
 	// validate the jwt
 	jwtParseReturnCode = jwt_decode(&jwt, jwtCookieValChrPtr, keyBinary, jwtcf->auth_jwt_key.len / 2);
 	if (jwtParseReturnCode != 0)
 	{
-		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "failed to parse jwt");
+          ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "failed to parse jwt: code %d", jwtParseReturnCode);
 		goto redirect;
 	}
-	
+
 //	ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "parsed jwt:\n%s", jwt_dump_str(jwt, 1));
-	
+
 	// validate the algorithm
 	alg = jwt_get_alg(jwt);
 	if (alg != JWT_ALG_HS256)
@@ -153,7 +153,7 @@ static ngx_int_t ngx_http_auth_jwt_handler(ngx_http_request_t *r)
 		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "invalid algorithm in jwt %d", alg);
 		goto redirect;
 	}
-	
+
 	// validate the exp date of the JWT
 	exp = (time_t)jwt_get_grant_int(jwt, "exp");
 	now = time(NULL);
@@ -162,13 +162,13 @@ static ngx_int_t ngx_http_auth_jwt_handler(ngx_http_request_t *r)
 		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "the jwt has expired");
 		goto redirect;
 	}
-	
+
 	return NGX_OK;
-	
+
 	redirect:
 		r->headers_out.location = ngx_list_push(&r->headers_out.headers);
-		
-		if (r->headers_out.location == NULL) 
+
+		if (r->headers_out.location == NULL)
 		{
 			ngx_http_finalize_request(r, NGX_HTTP_INTERNAL_SERVER_ERROR);
 		}
@@ -259,7 +259,7 @@ static ngx_int_t ngx_http_auth_jwt_init(ngx_conf_t *cf)
 	cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
 	h = ngx_array_push(&cmcf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
-	if (h == NULL) 
+	if (h == NULL)
 	{
 		return NGX_ERROR;
 	}
@@ -276,16 +276,16 @@ ngx_http_auth_jwt_create_loc_conf(ngx_conf_t *cf)
 	ngx_http_auth_jwt_loc_conf_t *conf;
 
 	conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_auth_jwt_loc_conf_t));
-	if (conf == NULL) 
+	if (conf == NULL)
 	{
 		return NULL;
 	}
-	
+
 	// set the flag to unset
 	conf->auth_jwt_enabled = (ngx_flag_t) -1;
 
 	ngx_conf_log_error(NGX_LOG_DEBUG, cf, 0, "Created Location Configuration");
-	
+
 	return conf;
 }
 
@@ -298,17 +298,17 @@ ngx_http_auth_jwt_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 
 	ngx_conf_merge_str_value(conf->auth_jwt_loginurl, prev->auth_jwt_loginurl, "");
 	ngx_conf_merge_str_value(conf->auth_jwt_key, prev->auth_jwt_key, "");
-	
-	
-	if (conf->auth_jwt_enabled == ((ngx_flag_t) -1)) 
+
+
+	if (conf->auth_jwt_enabled == ((ngx_flag_t) -1))
 	{
 		conf->auth_jwt_enabled = (prev->auth_jwt_enabled == ((ngx_flag_t) -1)) ? 0 : prev->auth_jwt_enabled;
 	}
-	
+
 	ngx_conf_log_error(NGX_LOG_DEBUG, cf, 0, "Merged Location Configuration");
 
-//	ngx_conf_log_error(NGX_LOG_ERR, cf, 0, "Key: %s, Enabled: %d", 
-//			conf->auth_jwt_key.data, 
+//	ngx_conf_log_error(NGX_LOG_ERR, cf, 0, "Key: %s, Enabled: %d",
+//			conf->auth_jwt_key.data,
 //			conf->auth_jwt_enabled);
 	return NGX_CONF_OK;
 }
@@ -329,16 +329,16 @@ hex_char_to_binary( char ch, char* ret )
 }
 
 static int
-hex_to_binary( const char* str, u_char* buf, int len ) 
+hex_to_binary( const char* str, u_char* buf, int len )
 {
-	u_char	
+	u_char
 		*cpy = buf;
 	char
 		low,
 		high;
 	int
 		odd = len % 2;
-	
+
 	if (odd) {
 		return -1;
 	}
@@ -346,7 +346,7 @@ hex_to_binary( const char* str, u_char* buf, int len )
 	for (int i = 0; i < len; i += 2) {
 		hex_char_to_binary( *(str + i), &high );
 		hex_char_to_binary( *(str + i + 1 ), &low );
-		
+
 		*cpy++ = low | (high << 4);
 	}
 	return 0;
@@ -360,5 +360,3 @@ static char* ngx_str_t_to_char_ptr(ngx_pool_t *pool, ngx_str_t str)
 	*(char_ptr + str.len) = '\0';
 	return char_ptr;
 }
-
-
